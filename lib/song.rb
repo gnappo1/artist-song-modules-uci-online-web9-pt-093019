8b3086e9391c739a31283fea7f29eca1edca551d
Matteo Piccini <gnappo.1@live.it>
2019-11-05 01:51:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,7 @@
 require 'pry'
+require_relative '../lib/concerns/findable'
+require_relative '../lib/concerns/memorable'
+require_relative '../lib/concerns/paramable'
 
 class Song
   attr_accessor :name
@@ -6,24 +9,8 @@ class Song
 
   @@songs = []
 
-  def initialize
-    @@songs << self
-  end
-
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
-
   def self.all
     @@songs
-  end
-
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
   end
 
   def artist=(artist)
