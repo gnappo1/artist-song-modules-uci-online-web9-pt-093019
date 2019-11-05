f38d057cf5fd099713409dbe1e805ae60adaa58e
Matteo Piccini <gnappo.1@live.it>
2019-11-05 01:53:00 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -7,16 +7,13 @@ class Artist
   attr_accessor :name
   attr_reader :songs
   include Memorable::InstanceMethods
+  include Findable::ClassMethods
   extend Memorable::ClassMethods
   @@artists = []
 
   def initialize
     super
     @songs = []
-  end
-
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
   end
 
   def self.all
