de0d59ccf51729e71237fb48539c362f386536fd
Matteo Piccini <gnappo.1@live.it>
2019-11-05 01:53:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,7 +7,7 @@ class Song
   attr_accessor :name
   attr_reader :artist
   include Memorable::InstanceMethods
-  include Findable::ClassMethods
+  extend Findable::ClassMethods
   extend Memorable::ClassMethods
   @@songs = []
 
